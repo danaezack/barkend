980307c453045b2224296a15cd61f6b3b9030d79
Dana Zack <danaezack@gmail.com>
2024-02-14 15:59:04 -0700

    Docs: set up backend directory

---
+++ barkend-backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
