3e8ff03d02ff91c6d3fe339d6c2c24c8ea18f730
Jamie Francisco <0jamiefrancisco@gmail.com>
2024-02-15 12:25:33 -0800

    Refactor: simplify Dogs component

--- barkend-frontend/src/components/Dogs/Dogs.js
+++ barkend-frontend/src/components/Dogs/Dogs.js
@@ -3,19 +3,17 @@ import DogCard from '../DogCard/DogCard.js'
 
 function Dogs({ dogs }) {
 
-  const dogCards = dogs.map(dog => {
-    return (
-      <DogCard
-        key={dog.id}
-        name={dog.name}
-      />
-    )
-  })
-
   return (
     <div className='dogs-container'>
       <h2>Dogs go here!</h2>
-      {dogCards}
+      {dogs.map(dog => {
+        return (
+          <DogCard
+            key={dog.id}
+            name={dog.name}
+          />
+        )
+      })}
     </div>
   )
 }
