cfcc4644d4a191836987d62d4ae99795436402a7
Jamie Francisco <0jamiefrancisco@gmail.com>
2024-02-17 17:51:46 -0800

    Feat: add toggle buttons and logic for filtering

--- barkend-frontend/src/components/Filter/Filter.jsx
+++ barkend-frontend/src/components/Filter/Filter.jsx
@@ -2,25 +2,47 @@ import './Filter.css';
 import { useState } from 'react';
 
 function Filter({ setDogs, cleanAnimals }) {
-  const [size, setSize] = useState('')
+  const [selectedSizes, setSelectedSizes] = useState([]);
 
-  const filterBySize = (event, size) => {
-    event.preventDefault()
-    const dogsBySize = cleanAnimals.filter(dog => dog.size === size);
+  const handleSizeToggle = (size) => {
+    setSelectedSizes(prevSizes => {
+      if (prevSizes.includes(size)) {
+        return prevSizes.filter(prevSize => prevSize !== size)
+      } else {
+        return [...prevSizes, size];
+      }
+    });
+    filterBySize();
+  }
+
+  const filterBySize = () => {
+    const dogsBySize = cleanAnimals.filter(dog => selectedSizes.includes(dog.size));
     setDogs(dogsBySize)
-    setSize('')
   }
 
   return (
-    <form className ='filter-container'>
-      <select className='dropdown' value={size} onChange={(event) => setSize(event.target.value) }>
-        <option defaultValue>Choose size</option>
-        <option value='Small'>Small</option>
-        <option value='Medium'>Medium</option>
-        <option value='Large'>Large</option>
-      </select>
-      <button className='filter-btn' type='submit' onClick={(event) => filterBySize(event, size)} >Select</button>
-    </form>
+    <div className ='filter-container'>
+      <div className='size-toggle-buttons'>
+      <button
+          className={`size-toggle-btn ${selectedSizes.includes('Small') ? 'active' : ''}`}
+          onClick={() => handleSizeToggle('Small')}
+        >
+          Small
+        </button>
+        <button
+          className={`size-toggle-btn ${selectedSizes.includes('Medium') ? 'active' : ''}`}
+          onClick={() => handleSizeToggle('Medium')}
+        >
+          Medium
+        </button>
+        <button
+          className={`size-toggle-btn ${selectedSizes.includes('Large') ? 'active' : ''}`}
+          onClick={() => handleSizeToggle('Large')}
+        >
+          Large
+        </button>
+        </div>
+    </div>
   );
 }
 
