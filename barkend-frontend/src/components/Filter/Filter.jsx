fe7e7a8830102492937295bf6b9a6bc88511360d
Jamie Francisco <0jamiefrancisco@gmail.com>
2024-02-19 13:42:06 -0800

    Feat: add knex.js and connect it to our postgreSQL db Co-authored by: Dana Zack <danaezack@gmail.com> Co-authored by: Ricky Tran <therichardtran@gmail.com>

--- barkend-frontend/src/components/Filter/Filter.jsx
+++ barkend-frontend/src/components/Filter/Filter.jsx
@@ -24,19 +24,19 @@ function Filter({ setDogs, cleanAnimals }) {
     <div className ='filter-container'>
       <div className='size-toggle-buttons'>
       <button
-          className={`size-toggle-btn ${selectedSizes.includes('Small') ? 'active' : ''}`}
+          className={`size-toggle-btn ${selectedSizes.includes('Small') && 'active'}`}
           onClick={() => handleSizeToggle('Small')}
         >
           Small
         </button>
         <button
-          className={`size-toggle-btn ${selectedSizes.includes('Medium') ? 'active' : ''}`}
+          className={`size-toggle-btn ${selectedSizes.includes('Medium') && 'active'}`}
           onClick={() => handleSizeToggle('Medium')}
         >
           Medium
         </button>
         <button
-          className={`size-toggle-btn ${selectedSizes.includes('Large') ? 'active' : ''}`}
+          className={`size-toggle-btn ${selectedSizes.includes('Large') && 'active'}`}
           onClick={() => handleSizeToggle('Large')}
         >
           Large
