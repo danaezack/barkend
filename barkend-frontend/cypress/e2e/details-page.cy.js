ebb2b902ac75134e7c9473386d730857485a5586
Dana Zack <danaezack@gmail.com>
2024-02-20 17:25:56 -0700

    Feat: initialize Cypress testing

---
+++ barkend-frontend/cypress/e2e/details-page.cy.js
@@ -0,0 +1,13 @@
+describe('User flows for home page', () => {
+  beforeEach(() => {
+    // cy.intercept('GET', '', {
+    //   statusCode: 200,
+    //   fixture: 'dogs'
+    // })
+    cy.visit('http://localhost:3000/')
+  })
+  
+  it('renders dog details page and expected dog details page elements', () => {
+    // cy.get()
+  })
+})
