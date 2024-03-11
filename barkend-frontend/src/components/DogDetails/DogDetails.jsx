481cd8a78ee564f585ec94723314a733662ba04f
Jamie Francisco <0jamiefrancisco@gmail.com>
2024-03-11 12:47:16 -0700

    Add more styling to dogcards and dogdetails

--- barkend-frontend/src/components/DogDetails/DogDetails.jsx
+++ barkend-frontend/src/components/DogDetails/DogDetails.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper/modules';
 import { dogShape } from '../../propTypes/dogShape.js';
+import Header from '../Header/Header.jsx';
 import xBtn from "../../images/x-lg.svg";
 import hrtBtn from "../../images/bookmark-heart.svg";
 import hrtBtnFill from "../../images/bookmark-heart-fill.svg";
@@ -11,7 +12,7 @@ import 'swiper/css/navigation';
 import 'swiper/css';
 import './DogDetails.css';
 
-function DogDetails({ allDogs }) {
+function DogDetails({ allDogs, viewAllDogs }) {
   const [isFavorited, setIsFavorited] = useState(false);
   const { id } = useParams();
   let dog = allDogs.find(dog => dog.id === Number(id));
@@ -25,39 +26,46 @@ function DogDetails({ allDogs }) {
   }
 
   return (
-    <div className="details-container">
-      <aside className="image-carousel">
-        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-          {dog.photos.map((image, index) => (
-            <SwiperSlide key={index}>
-              <img key={index} className='dog-img' src={image} alt=""/>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </aside>
-      <main className="dog-details">
-        <Link to={'/main'}>
-          <img className='x-btn' src={xBtn} alt="close button" />
-        </Link>
-        <img onClick={() => handleClick()} className={isFavorited ? 'hrt-btn hidden' : 'hrt-btn'} src={hrtBtn} alt="close button"/>
-        <img onClick={() => handleClick()} className={isFavorited ? 'hrt-btn-fill flip' : 'hrt-btn-fill hidden'} src={hrtBtnFill} alt="close button"/>
-        <div className="main-details-container">
-          <h1 className='name'>Hi, I'm {dog.name}.</h1>
-          <p className='sub-details'>{dog.age} • {dog.gender} • {dog.breeds} <br></br></p>
-          {dog && dog.environment && dog.environment.children && (
- <p>Good with Children</p>
-)}
-          <ul>{dog.name}'s Qualities:
-            <p className='tags'>{dog.tags}</p>
-          </ul>
-        </div>
-        <div className="inquire-container">
-          <h3 className='inquiry'>Interested in {dog.name}?</h3>
-          {dog.contact.email && <p className='sub-details'>email: {dog.contact.email}</p>}
-          {dog.contact.phone && <p className='sub-details'>phone: {dog.contact.phone}</p>}
-        </div>
-      </main>
-    </div>
+    <>
+      <Header viewAllDogs={viewAllDogs} />
+
+      <div className="details-container">
+          <div className="favorites-button-container">
+            <Link to={'/main'}>
+              <img className='x-btn' src={xBtn} alt="close button" />
+            </Link>
+            <img onClick={() => handleClick()} className={isFavorited ? 'hrt-btn' : 'hrt-btn'} src={hrtBtn} alt="close button" />
+
+            <img onClick={() => handleClick()} className={isFavorited ? 'hrt-btn-fill flip' : 'hrt-btn-fill'} src={hrtBtnFill} alt="close button" />
+          </div>
+        <aside className="image-carousel">
+          <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+            {dog.photos.map((image, index) => (
+              <SwiperSlide key={index}>
+                <img key={index} className='dog-img' src={image} alt="" />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </aside>
+        <main className="dog-details">
+          <div className="main-details-container">
+            <h1 className='name'>Hi, I'm {dog.name}.</h1>
+            <p className='sub-details'>{dog.age} • {dog.gender} • {dog.breeds} <br></br></p>
+            {dog && dog.environment && dog.environment.children && (
+              <p>Good with Children</p>
+            )}
+            <ul>{dog.name}'s Qualities:
+              <p className='tags'>{dog.tags}</p>
+            </ul>
+          </div>
+          <div className="inquire-container">
+            <h3 className='inquiry'>Interested in {dog.name}?</h3>
+            {dog.contact.email && <p className='sub-details'>email: {dog.contact.email}</p>}
+            {dog.contact.phone && <p className='sub-details'>phone: {dog.contact.phone}</p>}
+          </div>
+        </main>
+      </div>
+    </>
   );
 }
 
