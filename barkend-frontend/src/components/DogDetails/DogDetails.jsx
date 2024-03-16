bf9b7e43f7e84c509431b61e0c8e625b6c5f8d4b
Jamie Francisco <0jamiefrancisco@gmail.com>
2024-03-16 10:53:29 -0700

    Revert "Fix a couple of bugs from merge"
    This reverts commit 64555a70cfa6740cf304a955efb66ea5fe19115a.

--- barkend-frontend/src/components/DogDetails/DogDetails.jsx
+++ barkend-frontend/src/components/DogDetails/DogDetails.jsx
@@ -25,56 +25,42 @@ function DogDetails({ addFavorite, allDogs, viewAllDogs }) {
   }
 
   return (
-        <>
+            <>
       <Header viewAllDogs={viewAllDogs} />
-      <div className="dog-page-wrapper">
-        <div className="favorites-button-container">
-          <img onClick={() => handleClick()} className={dog.favorited ? 'hrt-btn hidden' : 'hrt-btn'} src={hrtBtn} alt="close button" />
-          <img onClick={() => handleClick()} className={dog.favorited ? 'hrt-btn-fill flip' : 'hrt-btn-fill hidden'} src={hrtBtnFill} alt="close button" />
-          <Link to={'/main'}>
-            <img className='x-btn' src={xBtn} alt="close button" />
-          </Link>
+    <div className="details-container">
+      <aside className="image-carousel">
+        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+          {dog.photos.map((image, index) => (
+            <SwiperSlide key={index}>
+              <img key={index} className='dog-img' src={image} alt=""/>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </aside>
+      <main className="dog-details">
+        <Link to={'/main'}>
+          <img className='x-btn' src={xBtn} alt="close button" />
+        </Link>
+        <img onClick={() => handleClick()} className={dog.favorited ? 'hrt-btn hidden' : 'hrt-btn'} src={hrtBtn} alt="close button"/>
+        <img onClick={() => handleClick()} className={dog.favorited ? 'hrt-btn-fill flip' : 'hrt-btn-fill hidden'} src={hrtBtnFill} alt="close button"/>
+        <div className="main-details-container">
+          <h1 className='name'>Hi, I'm {dog.name}.</h1>
+          <p className='sub-details'>{dog.age} • {dog.gender} • {dog.breeds} <br></br></p>
+          {dog && dog.environment && dog.environment.children && (
+ <p>Good with Children</p>
+)}
+          <ul>{dog.name}'s Qualities:
+            <p className='tags'>{dog.tags}</p>
+          </ul>
         </div>
-      </div>
-      <main className="dog-details-container" style={{ height: '100vh' }}>
-        <aside className="dog-details">
-          <div className="details-box">
-            <div className="main-details-container">
-              <h1 className='name'>Hi, I'm {dog.name}.</h1>
-              <p className='sub-details'> {dog.breeds} • {dog.age} • {dog.gender} <br></br></p>
-              <ul className="tags-list">MyQualities:
-                {dog && dog.description && (
-                <p className="description">{dog.description}</p> 
-              )}
-                <p className='tags'>{dog.tags.replace(/,/g, ' • ')}</p>
-              </ul>
-              {dog && dog.environment && dog.environment.children && (
-                <p className="children-info">Good with Children</p> 
-              )}
-
-              {dog && dog.attributes && dog.attributes.spayed_neutered && (
-                <p className="fixed-info">Fixed</p>
-                
-              )}
-            </div>
-          </div>
-          <div className="inquire-container">
-            <h3 className='inquiry'>Interested in {dog.name}?</h3>
-            {dog.contact.email && <p className='sub-details'>email: {dog.contact.email}</p>}
-            {dog.contact.phone && <p className='sub-details'>phone: {dog.contact.phone}</p>}
-          </div>
-        </aside>
-        <aside className="image-carousel">
-          <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-            {dog.photos.map((image, index) => (
-              <SwiperSlide key={index}>
-                <img key={index} className='dog-img' src={image} alt="" />
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </aside>
+        <div className="inquire-container">
+          <h3 className='inquiry'>Interested in {dog.name}?</h3>
+          {dog.contact.email && <p className='sub-details'>email: {dog.contact.email}</p>}
+          {dog.contact.phone && <p className='sub-details'>phone: {dog.contact.phone}</p>}
+        </div>
       </main>
-    </>
+    </div>
+      <>
   );
 }
 
